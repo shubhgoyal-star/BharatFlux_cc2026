ccb05107450d090ceacefc2e88ff40a325490366
Ansh Pandya <ansh.pandya@avantika.edu.in>
2026-05-12 17:52:11 +0000

    function to make the road

--- ansh-work/sketch.js
+++ ansh-work/sketch.js
@@ -1,7 +1,22 @@
 function setup() {
-    createCanvas(800, 600);
+    createCanvas(1000, 600);
 }
 
 function draw() {
-    background(220);
+    background(30);
+    drawRoads()
+
+}
+// function to make the roads
+function drawRoads() {
+
+  // Background of road 
+  fill(50);
+  noStroke();
+
+  // Highway with 4 lanes 
+  rect(180, 0, 140, 600);
+  rect(340, 0, 140, 600);
+  rect(500, 0, 140, 600);
+  rect(660, 0, 140, 600);
 }
