c741826d68b17e7e5c04962161709dfd488da6c2
Ansh Pandya <ansh.pandya@avantika.edu.in>
2026-05-12 17:59:30 +0000

    change the width of the divider/lines

--- ansh-work/sketch.js
+++ ansh-work/sketch.js
@@ -21,7 +21,7 @@ function drawRoads() {
   rect(660, 0, 140, 600);
 //   line to divide the road 
   stroke(255);
-  strokeWeight(2);
+  strokeWeight(5);
 
   line(340, 0, 340, 600);
   line(500, 0, 500, 600);
