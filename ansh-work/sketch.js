0e1b2bae1e1987ee2615ce0a45f9bcbc004d93e9
Ansh Pandya <ansh.pandya@avantika.edu.in>
2026-05-13 07:22:39 +0000

    added traffic density controller

--- ansh-work/sketch.js
+++ ansh-work/sketch.js
@@ -1,107 +1,191 @@
 // let carY = 600
+
 let cars = [];
+
 let laneTypes = ["FASTag", "MANUAL", "FASTag", "MANUAL"];
+
+let densitySlider;
+
 function setup() { 
+
     createCanvas(1000, 600);
+
+    densitySlider = createSlider(20, 100, 50);
+
+    densitySlider.position(20, 620);
+
+    densitySlider.size(200);
+
 }
 
 function draw() {
+
     background(30);
-    drawRoads()
+
+    drawRoads();
+
     // drawCar()
+
     spawnCars();
+
     moveCars();
+
     displayCars();
 
+    fill(255);
+
+    noStroke();
+
+    textSize(18);
+
+    textAlign(LEFT);
+
+    text(
+      "Traffic Density : " + densitySlider.value(),
+      20,
+      590
+    );
+
 }
+
 // function to make the roads
+
 function drawRoads() {
 
   // Background of road 
   fill(50);
+
   noStroke();
 
   // Highway with 4 lanes 
   rect(180, 0, 140, 600);
+
   rect(340, 0, 140, 600);
+
   rect(500, 0, 140, 600);
+
   rect(660, 0, 140, 600);
-//   line to divide the road 
+
+  //   line to divide the road 
+
   stroke(255);
+
   strokeWeight(5);
 
   line(340, 0, 340, 600);
+
   line(500, 0, 500, 600);
+
   line(660, 0, 660, 600);
+
   fill(15, 107, 62);
+
   noStroke();
-// making the hording / rectangular heading  of toll 
+
+  // making the hording / rectangular heading  of toll 
+
   rect(180, 0, 140, 60);
+
   rect(340, 0, 140, 60);
+
   rect(500, 0, 140, 60);
+
   rect(660, 0, 140, 60);
 
   //   this will add the text on the hording 
+
   fill(255);
+
   textSize(22);
+
   textAlign(CENTER, CENTER);
   
-//   text("FASTag", 250, 30);
-//   text("MANUAL", 410, 30);
-//   text("FASTag", 570, 30);
-//   text("MANUAL", 730, 30);
+  //   text("FASTag", 250, 30);
 
+  //   text("MANUAL", 410, 30);
 
-// Lane 1
-let laneTextX = [250, 410, 570, 730];
+  //   text("FASTag", 570, 30);
 
-for (let i = 0; i < 4; i++) {
+  //   text("MANUAL", 730, 30);
+
+  // Lane 1
+
+  let laneTextX = [250, 410, 570, 730];
+
+  for (let i = 0; i < 4; i++) {
   
-  text(laneTypes[i], laneTextX[i], 30);
+    text(laneTypes[i], laneTextX[i], 30);
   
-}
-//  Barrier 
-stroke(255, 0, 0);
-strokeWeight(6);
+  }
 
-line(200, 80, 300, 80);
+  //  Barrier 
 
-// Lane 2
-line(360, 80, 460, 80);
+  stroke(255, 0, 0);
 
-// Lane 3
-line(520, 80, 620, 80);
+  strokeWeight(6);
 
-// Lane 4
-line(680, 80, 780, 80);
+  line(200, 80, 300, 80);
+
+  // Lane 2
+
+  line(360, 80, 460, 80);
+
+  // Lane 3
+
+  line(520, 80, 620, 80);
+
+  // Lane 4
+
+  line(680, 80, 780, 80);
 
 }
 
 // Function to make car 
 
 // function drawCar(){
+
 //   fill(255, 140, 0);
+
 //   noStroke();
 
 //   rect(220, carY, 60, 100, 10);
+
 // //   Condition to move the car 
+
 //   carY = carY-3;
 
 // // Condition to start the car from 600 again 
+
 //     if(carY<-100){
+
 //         carY = 600
+
 //     }
+
 // }
 
 function spawnCars() {
 
-  if (frameCount % 40 === 0) {
+  let spawnRate = map(
+    densitySlider.value(),
+    20,
+    100,
+    70,
+    15
+  );
+
+  if (frameCount % floor(spawnRate) === 0) {
 
     let laneData = [
+
       { x: 220, lane: 0 },
+
       { x: 380, lane: 1 },
+
       { x: 540, lane: 2 },
+
       { x: 700, lane: 3 }
+
     ];
 
     let randomLane = random(laneData);
@@ -114,20 +198,28 @@ function spawnCars() {
         car.lane === randomLane.lane &&
         car.y > 500
       ) {
+
         canSpawn = false;
+
       }
 
     }
 
     if (canSpawn) {
 
-    let car = {
-      x: randomLane.x,
-      y: 650,
-      speed: random(3, 5),
-      lane: randomLane.lane,
-      waitTimer: 0
-};
+      let car = {
+
+        x: randomLane.x,
+
+        y: 650,
+
+        speed: random(3, 5),
+
+        lane: randomLane.lane,
+
+        waitTimer: 0
+
+      };
 
       cars.push(car);
 
@@ -136,16 +228,19 @@ function spawnCars() {
   }
 
 }
+
 function moveCars() {
 
   for (let i = 0; i < cars.length; i++) {
 
     let car = cars[i];
+
     let laneType = laneTypes[car.lane];
 
     let targetSpeed = car.speed;
 
     // Queue system
+
     for (let j = 0; j < cars.length; j++) {
 
       let other = cars[j];
@@ -166,17 +261,21 @@ function moveCars() {
     // ===== TOLL STOPPING LOGIC =====
 
     // MANUAL lanes
+
     if (laneType === "MANUAL") {
 
       // Stop near barrier
+
       if (car.y < 140 && car.y > 90) {
 
         targetSpeed = 0;
 
         // Wait longer
+
         car.waitTimer++;
 
         // After waiting enough
+
         if (car.waitTimer > 80) {
 
           targetSpeed = 2;
@@ -188,9 +287,11 @@ function moveCars() {
     }
 
     // FASTag lanes
+
     else if (laneType === "FASTag") {
 
       // Small slow zone
+
       if (car.y < 140 && car.y > 90) {
 
         targetSpeed = 2;
@@ -203,12 +304,18 @@ function moveCars() {
 
   }
 
+  // Remove cars after leaving screen
+
+  cars = cars.filter(car => car.y > -120);
+
 }
+
 function displayCars() {
 
   for (let car of cars) {
 
     fill(255, 140, 0);
+
     noStroke();
 
     rect(car.x, car.y, 60, 100, 10);
@@ -216,6 +323,7 @@ function displayCars() {
   }
 
 } 
+
 function mousePressed() {
 
   let laneStartX = [180, 340, 500, 660];
@@ -234,7 +342,10 @@ function mousePressed() {
     }
 
   }
-  function toggleLane(laneIndex) {
+
+}
+
+function toggleLane(laneIndex) {
 
   if (laneTypes[laneIndex] === "FASTag") {
 
@@ -249,7 +360,3 @@ function mousePressed() {
   }
 
 }
-
-}
-
-
